c27ed77e9f825a90533cb57479f14d7043e37f30
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-07-14 22:27:10 -0400

    Update rails_helper.rb file with correct content

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,21 +1,16 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "spec/examples.txt"
-  config.disable_monkey_patching!
-
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
-
-  config.order = :random
-  Kernel.srand config.seed
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
 end
