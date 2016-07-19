7c465e5b6623bb7bafdb0dc16a94561472126ffb
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-07-19 16:36:32 -0400

    Update default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = "trialbywhiteboardrailsapi.herokuapp.com"
+  Rails.application.routes.default_url_options = {
+    host: "trialbywhiteboardrailsapi.herokuapp.com",
+    protocol: "https"
+  }
 end
